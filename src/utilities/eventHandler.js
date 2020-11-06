b3588572d66f3433525c8b36a06aefc76668fa0e
brettsodie <blsodie@gmail.com>
2020-11-05 20:50:14 -0500

    fix: remove console log in event handler

--- src/utilities/eventHandler.js
+++ src/utilities/eventHandler.js
@@ -1,15 +1,18 @@
 const localSockets = require('../localSockets');
 
-module.exports = (message) => {
+module.exports = message => {
   const obj = JSON.parse(message);
   console.log(obj);
   console.log(localSockets);
 
   obj._ids.forEach(_id => {
     const session = localSockets.active[_id];
-    console.log(session.app_ids[obj.app_id])
+    
     if (session && (session.app_ids[obj.app_id] || obj.subscribed)) {
       global.io.to(session.socket_id).emit('PLAY_AUDIO', { audio: obj.audio});
+      if (obj.subscribed) {
+        session.app_ids[obj.app_id] = true;
+      }
     }
   });
 };
